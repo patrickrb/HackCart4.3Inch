e0825e71f82bee3f96f7323366ad46748616a28d
Patrick Burns <burnsoft@gmail.com>
2023-10-27 20:19:06 -0500

    dashboard is animating on the display now

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,11 @@
-//lvgl
+// lvgl
 #include <lvgl.h>
-#include <demos/lv_demos.h>
-//UI
-#include "ui.h"
-static int first_flag = 0;
-extern int zero_clean;
-extern int goto_widget_flag;
-extern int bar_flag;
-extern lv_obj_t * ui_MENU;
-extern lv_obj_t * ui_TOUCH;
-extern lv_obj_t * ui_JIAOZHUN;
-extern lv_obj_t * ui_Label2;
-extern lv_obj_t * ui_touch_calibrate;//校准界面
-static lv_obj_t * ui_Label;//TOUCH界面label
-static lv_obj_t * ui_Label3;//TOUCH界面label3
-static lv_obj_t * ui_Labe2;//Menu界面进度条label
-static lv_obj_t * bar;//Menu界面进度条
+// UI
+#include <ui.h>
+#include "UI/ui_helpers.h"
 
 uint16_t touchCalibration_x0 = 300, touchCalibration_x1 = 3600, touchCalibration_y0 = 300, touchCalibration_y1 = 3600;
-uint8_t  touchCalibration_rotate = 1, touchCalibration_invert_x = 2, touchCalibration_invert_y = 0;
+uint8_t touchCalibration_rotate = 1, touchCalibration_invert_x = 2, touchCalibration_invert_y = 0;
 static int val = 100;
 
 static uint32_t screenWidth;
@@ -30,26 +17,24 @@ static lv_disp_drv_t disp_drv;
 #define TFT_BL 2
 #define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 
-
 #if defined(DISPLAY_DEV_KIT)
 Arduino_GFX *lcd = create_default_Arduino_GFX();
-#else /* !defined(DISPLAY_DEV_KIT) */
-
+#else  /* !defined(DISPLAY_DEV_KIT) */
 
 Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-  GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
-  40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
-  45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
-  5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
-  8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
+    GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
+    40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
+    45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
+    5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
+    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
 );
 // option 1:
 // ILI6485 LCD 480x272
 Arduino_RPi_DPI_RGBPanel *lcd = new Arduino_RPi_DPI_RGBPanel(
-  bus,
-  480 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 2 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
-  272 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 2 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
-  1 /* pclk_active_neg */, 7000000 /* prefer_speed */, true /* auto_flush */);
+    bus,
+    480 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 2 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
+    272 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 2 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
+    1 /* pclk_active_neg */, 7000000 /* prefer_speed */, true /* auto_flush */);
 #endif /* !defined(DISPLAY_DEV_KIT) */
 #include "touch.h"
 /* Display flushing */
@@ -78,11 +63,11 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
       /*Set the coordinates*/
       data->point.x = touch_last_x;
       data->point.y = touch_last_y;
-      Serial.print( "Data x :" );
-      Serial.println( touch_last_x );
+      Serial.print("Data x :");
+      Serial.println(touch_last_x);
 
-      Serial.print( "Data y :" );
-      Serial.println( touch_last_y );
+      Serial.print("Data y :");
+      Serial.println(touch_last_y);
     }
     else if (touch_released())
     {
@@ -95,102 +80,82 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
   }
 }
 
-//触摸Label控件
-void label_xy()
+int speedValue = 0;
+bool incrementingSpeed = true;
+const int speedInterval = 30;
+const int SPEED_INCREMENT = 1;
+const int MAX_SPEED = 45;
+const int MIN_SPEED = 0;
+
+float fraction = 0.0f; // Fraction of the animation cycle, varies from 0 to 1 and back to 0
+
+void updateSpeed()
 {
-  ui_Label = lv_label_create(ui_TOUCH);
-  lv_obj_enable_style_refresh(true);
-  lv_obj_set_width(ui_Label, LV_SIZE_CONTENT);   /// 1
-  lv_obj_set_height(ui_Label, LV_SIZE_CONTENT);    /// 1
-  lv_obj_set_x(ui_Label, -35);
-  lv_obj_set_y(ui_Label, -40);
-  lv_obj_set_align(ui_Label, LV_ALIGN_CENTER);
-  lv_obj_set_style_text_color(ui_Label, lv_color_hex(0xFF0000), LV_PART_MAIN | LV_STATE_DEFAULT);
-  lv_obj_set_style_text_opa(ui_Label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-  lv_obj_set_style_text_font(ui_Label, &lv_font_montserrat_16, LV_PART_MAIN | LV_STATE_DEFAULT);
+  const float FRACTION_INCREMENT = 0.005f;
 
-  ui_Label3 = lv_label_create(ui_TOUCH);
-  lv_obj_enable_style_refresh(true);
-  lv_obj_set_width(ui_Label3, LV_SIZE_CONTENT);   /// 1
-  lv_obj_set_height(ui_Label3, LV_SIZE_CONTENT);    /// 1
-  lv_obj_set_x(ui_Label3, 65);
-  lv_obj_set_y(ui_Label3, -40);
-  lv_obj_set_align(ui_Label3, LV_ALIGN_CENTER);
-  lv_obj_set_style_text_color(ui_Label3, lv_color_hex(0x00FF00), LV_PART_MAIN | LV_STATE_DEFAULT);
-  lv_obj_set_style_text_opa(ui_Label3, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-  lv_obj_set_style_text_font(ui_Label3, &lv_font_montserrat_16, LV_PART_MAIN | LV_STATE_DEFAULT);
-}
-
-
-//进度条控件
-void lv_example_bar(void)
-{
-  //////////////////////////////
-  bar = lv_bar_create(ui_MENU);
-  lv_bar_set_value(bar, 0, LV_ANIM_OFF);
-  lv_obj_set_width(bar, 300);
-  lv_obj_set_height(bar, 20);
-  lv_obj_set_x(bar, 0);
-  lv_obj_set_y(bar, 90);
-  lv_obj_set_align(bar, LV_ALIGN_CENTER);
-  lv_obj_set_style_bg_img_src(bar, &ui_img_bar_480_01_png, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-  lv_obj_set_style_bg_img_src(bar, &ui_img_bar_480_02_png, LV_PART_INDICATOR | LV_STATE_DEFAULT);
-  lv_obj_set_style_outline_color(bar, lv_color_hex(0x2D8812), LV_PART_INDICATOR | LV_STATE_DEFAULT);
-  lv_obj_set_style_outline_opa(bar, 255, LV_PART_INDICATOR | LV_STATE_DEFAULT);
-  //////////////////////
-  ui_Labe2 = lv_label_create(bar);//创建标签
-  lv_obj_set_style_text_color(ui_Labe2, lv_color_hex(0x09BEFB), LV_STATE_DEFAULT);
-  lv_label_set_text(ui_Labe2, "0%");
-  lv_obj_center(ui_Labe2);
-}
-
-
-uint16_t calData[5] = { 190, 3679, 382, 3335, 0 };
-
-void callback1()  //Callback function
-{
-  if (bar_flag == 6)
+  if (incrementingSpeed)
   {
-    if (val > 1)
+    fraction += FRACTION_INCREMENT;
+    if (fraction >= 1.0f)
     {
-      val--;
-      lv_bar_set_value(bar, val, LV_ANIM_OFF);
-      lv_label_set_text_fmt(ui_Labe2, "%d %%", val);
-    }
-    else
-    {
-      lv_obj_clear_flag(ui_touch, LV_OBJ_FLAG_CLICKABLE);
-      lv_label_set_text(ui_Labe2, "Loading");
-      delay(150);
-      val = 100;
-      bar_flag = 0; //停止进度条标志
-      goto_widget_flag = 1; //进入widget标志
-
+      fraction = 1.0f;
+      incrementingSpeed = false;
     }
   }
+  else
+  {
+    fraction -= FRACTION_INCREMENT;
+    if (fraction <= 0.0f)
+    {
+      fraction = 0.0f;
+      incrementingSpeed = true;
+    }
+  }
+
+  // Applying the sine function for ease-in-out effect over the complete cycle
+  speedValue = (int)(MAX_SPEED * (sin((fraction - 0.5f) * M_PI) + 1) / 2);
+
+  lv_arc_set_value(ui_ArcSpeed, speedValue);
+  lv_label_set_text_fmt(ui_LabelSpeed, "%d", speedValue);
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin( 9600 ); /*初始化串口*/
+void lvgl_loop(void *parameter)
+{
+  while (true)
+  {
+    lv_timer_handler();
+  }
+  vTaskDelete(NULL);
+}
 
-  //lvgl初始化
-  lv_init();
-  // Init Display
+void guiHandler()
+{
+  xTaskCreatePinnedToCore(
+      lvgl_loop,   // Function that should be called
+      "LVGL Loop", // Name of the task (for debugging)
+      20480,       // Stack size (bytes)
+      NULL,        // Parameter to pass
+      1,           // Task priority
+      NULL,        // Task handle
+      1);
+}
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200); // Init Display
+
   lcd->begin();
-  lcd->setTextSize(2);
   lcd->fillScreen(BLACK);
-  //触摸初始化
+  lcd->setTextSize(2);
+  delay(200);
+
+  lv_init();
   touch_init();
-  delay(300);
-#ifdef TFT_BL
-  pinMode(TFT_BL, OUTPUT);
-  digitalWrite(TFT_BL, HIGH);
-#endif
   screenWidth = lcd->width();
   screenHeight = lcd->height();
-  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 8);
+
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 10);
 
   /* Initialize the display */
   lv_disp_drv_init(&disp_drv);
@@ -208,47 +173,30 @@ void setup() {
   indev_drv.read_cb = my_touchpad_read;
   lv_indev_drv_register(&indev_drv);
 
-   lv_demo_widgets();
-  //  lcd->fillScreen(RED);
-  //  delay(2000);
-  //  lcd->fillScreen(GREEN);
-  //  delay(2000);
-  //  lcd->fillScreen(BLUE);
-  //  delay(2000);
-  //  lcd->fillScreen(WHITE);
-  //  delay(2000);
-  //  lcd->fillScreen(BLACK);
-  //  delay(2000);
-  // ui_init();//开机UI界面
-  //  lv_timer_handler();
-  //  delay(3000);
-  //  lcd->fillScreen(BLACK);
-  //  lcd->setCursor(120, 120);
-  //  lcd->printf("Please touch!");
+  // Back light
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
 
+  lcd->fillScreen(BLACK);
+
+  ui_init();
+  guiHandler();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  lv_timer_handler();
-  delay(5);
-  //  if (touch_touched())
-  //  {
-  //
-  //    lcd->fillScreen(BLACK);
-  //    lcd->setCursor(140, 120);
-  //    lcd->printf("               ");
-  //    lcd->setCursor(140, 150);
-  //    lcd->printf("               ");
-  //
-  //    lcd->setCursor(120, 120);
-  //    lcd->printf("x: %i     ", touch_last_x);
-  //    lcd->setCursor(120, 150);
-  //    lcd->printf("y: %i    ", touch_last_y);
-  //    Serial.print( "Data x :" );
-  //    Serial.println( touch_last_x );
-  //    Serial.print( "Data y :" );
-  //    Serial.println( touch_last_y );
-  //
-  //  }
+unsigned long previousMillis = 0;
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+
+  // Calculate the time elapsed since the last update
+  unsigned long timeElapsed = currentMillis - previousMillis;
+
+  // Update speed based on elapsed time
+  while (timeElapsed >= speedInterval)
+  {
+    updateSpeed();
+    timeElapsed -= speedInterval;
+    previousMillis += speedInterval;
+  }
 }
